2a098e92dcec6a13064ef44632165d41f417fddc
XuPeiYao <st89382000@gmail.com>
2018-01-06 02:02:07 +0800

    加入載入失敗重載

--- src/app/document-view/document-view/document-view.component.ts
+++ src/app/document-view/document-view/document-view.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { ViewProviders } from '../models/ViewProviders';
 import { UriBuilder } from 'uribuilder';
 import { cleanPath } from 'cleanPath';
+import { Observable } from 'rxjs/Observable';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
+import 'rxjs/operator/toPromise';
+import 'rxjs/operator/takeUntil';
 @Component({
   selector: 'ngx-document-view',
   templateUrl: './document-view.component.html',
   styleUrls: ['./document-view.component.css']
 })
 export class DocumentViewComponent implements OnInit {
+  @ViewChild('iframe') iframe: ElementRef;
+
   /**
    * 檢視器提供者
    */
@@ -23,10 +30,36 @@ export class DocumentViewComponent implements OnInit {
    */
   @Input() public frameBorder = 0;
 
+  public get iframeUrl(): string {
+    if (this.provider === ViewProviders.Google) {
+      return (
+        'http://docs.google.com/gview?url=' +
+        encodeURI(this.src) +
+        '&embedded=true'
+      );
+    } else if (this.provider === ViewProviders.Microsoft) {
+      return (
+        'https://view.officeapps.live.com/op/embed.aspx?src=' +
+        encodeURI(this.src)
+      );
+    }
+
+    return null;
+  }
   constructor() {}
-
-  ngOnInit() {}
-
+  loadOK = false;
+  ngOnInit() {
+    const sub = TimerObservable.create(6000, 6000).subscribe(x => {
+      if (this.loadOK) {
+        sub.unsubscribe();
+        return;
+      }
+      (this.iframe.nativeElement as HTMLIFrameElement).src = this.iframeUrl;
+    });
+  }
+  console() {
+    console.log('GOGO');
+  }
   /**
    * 轉換連結與編碼
    * @param url 連結
